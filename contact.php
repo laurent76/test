9818e04e973f06e3fdeb952a734c623da79ed5e3
laurent76 <lau76120@gmail.com>
2016-11-23 10:57:14 +0100

    commit apres reset

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php echo "contact"; ?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>contact</title>
+  </head>
+  <body>
+    <p>contact</p>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 //test
-
+echo 'michel';
  ?>
